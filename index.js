44cdbcfce199ad046d5350aa186f91a1ff94c751
zeeshan1709 <159762677+zeeshan1709@users.noreply.github.com>
2024-02-14 18:00:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let currentPage = 1;
 const progress = document.getElementById('progress');
 
+
+updateProgressBar();
+
 function nextPage() {
   if (currentPage === 1) {
     if (!validateStep1()) {
@@ -13,20 +16,22 @@ function nextPage() {
   }
 
   currentPage++;
-  showPage(currentPage);
   updateProgressBar();
+  showPage(currentPage);
 }
 
 function previousPage() {
   currentPage--;
-  showPage(currentPage);
   updateProgressBar();
+  showPage(currentPage);
 }
+
 
 function validateStep1() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const confirmPassword = document.getElementById('confirm-password').value;
+  const tickIcon = document.getElementById('tick-icon');
 
   if (!validateEmail(email)) {
     alert('Please enter a valid email address.');
@@ -38,13 +43,31 @@ function validateStep1() {
     return false;
   }
 
-  if (password !== confirmPassword) {
-    alert('Password and confirm password do not match.');
+  const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+  const uppercaseRegex = /[A-Z]/;
+  const lowercaseRegex = /[a-z]/;
+  const numberRegex = /[0-9]/;
+
+  if (!specialCharRegex.test(password) ||
+      !uppercaseRegex.test(password) ||
+      !lowercaseRegex.test(password) ||
+      !numberRegex.test(password)) {
+    alert('Password must contain at least one special character, one uppercase letter, one lowercase letter, and one number.');
     return false;
   }
 
+  if (password !== confirmPassword) {
+    alert('Password and confirm password do not match.');
+    tickIcon.style.display = 'none'; 
+    return false;
+  }
+
+  tickIcon.style.display = 'inline'; 
   return true;
 }
+
+
+
 
 function validateStep2() {
   const fullname = document.getElementById('fullname').value;
